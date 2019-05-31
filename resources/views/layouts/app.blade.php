fcdad6300291c9a1074c7304d0f7dc620c5002db
Christian Castro <christian.castro@interpuerto.com>
2019-05-30 22:03:24 -0500

    1.0.7
    Frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="">{{ config('app.name', 'Laravel') }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -31,50 +31,19 @@
                 <a class="nav-link" href="#">{{ config('app.name', 'Laravel') }} <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    @can('users.index')
-                        <a class="dropdown-item" href="{{ route('users.index') }}">Usuarios</a>
-                    @endcan
-                    @can('roles.index')
-                        <a class="dropdown-item" href="{{ route('roles.index') }}">Roles</a>
-                    @endcan                
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
+                @can('users.index')
+                    <a class="dropdown-item" href="{{ route('users.index') }}">Usuarios</a>
+                @endcan
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                 @can('roles.index')
+                        <a class="dropdown-item" href="{{ route('roles.index') }}">Roles</a>
+                    @endcan   
             </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                @guest
-                    <a href="{{ route('login') }}">Login</a>
-                    <a href="{{ route('register') }}">Register</a>
-                @else
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-                    <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
+            
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                @endguest
-            </div>
+            </ul>
+            
             <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
@@ -105,15 +74,23 @@
         </div>
     </nav>
     <div class="d-flex">
-        <nav class="sidebar bg-dark">
+        <nav class="sidebar bg-light">
             <ul class="list-unstyled">
-                <li>Prueba</li>
+                <li class="nav-item">@can('users.index')
+                        <a class="dropdown-item" href="{{ route('users.index') }}">Usuarios</a>
+                    @endcan     
+                </li>
+                <li class="nav-item">
+                    @can('roles.index')
+                        <a class="dropdown-item" href="{{ route('roles.index') }}">Roles</a>
+                    @endcan 
+                </li>
             </ul>
         </nav>
         <div class="content p-4">
             <main class="contenido">                
                 <!--/row-->
-                <div class="row placeholders mb-3">
+                <div class="row  mb-3">
                     {{-- Contenido --}}
                     @yield('content')
                     {{-- /Contenido --}}
